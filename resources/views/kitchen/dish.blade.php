678db71eb756e84f619c4f78fae95bbcf527d35d
== <thawhtooin@gmail.com>
2023-04-15 09:41:06 +0630

    auth setup, adminlte setup

---
+++ resources/views/kitchen/dish.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.master')
+
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Kitchen Panel</h1>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+<!-- Main content -->
+<div class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-header">
+          <h3 class="card-title">DataTable with default features</h3>
+          </div>
+
+          <div class="card-body">
+            <table id="dishes" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>Rendering engine</th>
+                  <th>Browser</th>
+                  <th>Platform(s)</th>
+                  <th>Engine version</th>
+                  <th>CSS grade</th>
+                </tr>
+              </thead>
+                <tr>
+                  <td>Other browsers</td>
+                  <td>All others</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>U</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <!-- /.col-md-6 -->
+    </div>
+    <!-- /.row -->
+  </div><!-- /.container-fluid -->
+</div>
+<!-- /.content -->
+
+</div>
+<!-- /.content-wrapper -->
+@endsection
+<script type="text/javascript">
+$(function(){
+  $('dishes').DataTable({
+    "Paging":true,
+    "lengthChanging":false,
+    "searching":false,
+    "ordering":ture,
+    "info":true,
+    "autoWidth":false,
+  })
+});
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DishesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [App\Http\Controllers\OrderController::class, 'index'])->name('home');
+Route::resource('dish', App\Http\Controllers\DishesController::class);
+
+Auth::routes([
+  'register' => false,
+  'reset' => false,
+  'verify' => false,
+  'confirm' => false,
+]);
